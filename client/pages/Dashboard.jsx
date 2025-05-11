da0e02ff1f8094c7fcca5c301efc184e49a9bf78
Promit Chaudhuri <134211908+ProSkywalker16@users.noreply.github.com>
2025-05-12 00:38:25 +0530

    commit in progress

--- client/pages/Dashboard.jsx
+++ client/pages/Dashboard.jsx
@@ -38,7 +38,7 @@ const Dashboard = () => {
 
   const fetchLogs = async () => {
     try {
-      const res = await fetch("http://192.168.0.182:5000/data");     // honurag port
+      const res = await fetch("http://192.168.0.170:5000/data");     // honurag port
       //("http://192.168.0.170:5000/data");    // proskywalker port
            
       const data = await res.json();
